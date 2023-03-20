f9756818277c05d6a74185863a7049b97a150b35
FARAULT Theo <o22100566@info21-22.iut45.univ-orleans.fr>
2023-03-20 15:26:01 +0100

    ajout bd 2

--- Q2/bd.sql
+++ Q2/bd.sql
@@ -1,22 +1,37 @@
+DROP TABLE VOL;
+DROP TYPE EQUIPAGE FORCE;
+DROP TYPE QUALITES FORCE;
+DROP TYPE equipageTabT FORCE;
+DROP TYPE QUALITES_LIST FORCE;
 
-CREATE TYPE EQUIPAGE as object(
+
+
+create or replace type EQUIPAGE as object(
     nomE VARCHAR2(50),
     fonctionE VARCHAR2(50)
 );
-
-CREATE TYPE QUALITES as object(
+/
+create type equipageTabT as table of EQUIPAGE;
+/
+create or replace type QUALITES as object(
     nomQualite VARCHAR2(50),
     indiceQualite NUMBER,
     nb_votes NUMBER
 );
-
+/
+CREATE TYPE QUALITES_LIST AS VARRAY(10) OF QUALITES;
+/
 CREATE TABLE VOL(
     numVol NUMBER,
     dateheureDEP DATE,
     dateheureARR DATE,
     AeroportDEP VARCHAR2(100),
     AeroportARR VARCHAR2(100),
-    Equipage EQUIPAGE,
-    IndicesQualite QUALITES,
+    Equipage equipageTabT,
+    IndicesQualite QUALITES_LIST,
     CONSTRAINT vol_pk PRIMARY KEY (numVol)
-);
+)
+nested table Equipage store as Equipage_nt;
+
+INSERT INTO VOL values('2556', TO_DATE('2023-07-10 23:30:00', 'YYYY-MM-DD HH24:MI:SS'),TO_DATE('2023-07-11 05:30:00', 'YYYY-MM-DD HH24:MI:SS'), 'Charles de Gaulle','Antonio Carlos Jobim',
+    equipageTabT(EQUIPAGE('Jean', 'conducteur'), EQUIPAGE('George', 'aspirateurman')),QUALITES_LIST(QUALITES('securite', 2,5), QUALITES('Carbone', 2,5)) );
