4df3fe270b5597abfe838568632e56214bb5795f
FARAULT Theo <o22100566@info21-23.iut45.univ-orleans.fr>
2023-03-22 11:55:41 +0100

    [UPDATE] correction insert

--- Q2/insert.sql
+++ Q2/insert.sql
@@ -10,4 +10,4 @@ QUALITES_LIST(QUALITES('securite', 2,5), QUALITES('Carbone', 2,5)) );
 
 INSERT INTO VOL values('2557', TO_DATE('2023-07-12 10:30:00', 'YYYY-MM-DD HH24:MI:SS'),TO_DATE('2023-07-12 12:30:00', 'YYYY-MM-DD HH24:MI:SS'), 'Charles de Gaulle','Antonio Carlos Jobim',
 equipageTabT(EQUIPAGE('Jean', 'conducteur'), EQUIPAGE('Hugette', 'femme de ménage'),EQUIPAGE('Théo', 'conducteur')),
-QUALITES_LIST(QUALITES('securite', 2,5), QUALITES('Carbone', 2,5)) );
+QUALITES_LIST(QUALITES('securite', 1,5), QUALITES('Carbone', 3,5)) );
