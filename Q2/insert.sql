476c272b0af1387f6830a7757894958729eaf673
FARAULT Theo <o22100566@info21-23.iut45.univ-orleans.fr>
2023-03-22 11:45:59 +0100

    [ADD] requete c et d

--- Q2/insert.sql
+++ Q2/insert.sql
@@ -1,3 +1,9 @@
-INSERT INTO VOL values('2556', TO_DATE('2023-07-10 23:30:00', 'YYYY-MM-DD HH24:MI:SS'),TO_DATE('2023-07-11 05:30:00', 'YYYY-MM-DD HH24:MI:SS'), 'Charles de Gaulle','Antonio Carlos Jobim',
+
+
+INSERT INTO VOL values('2556', 
+TO_DATE('2023-07-10 23:30:00', 'YYYY-MM-DD HH24:MI:SS'),
+TO_DATE('2023-07-11 05:30:00', 'YYYY-MM-DD HH24:MI:SS'), 
+'Charles de Gaulle',
+'Antonio Carlos Jobim',
 equipageTabT(EQUIPAGE('Jean', 'conducteur'), EQUIPAGE('George', 'femme de ménage'),EQUIPAGE('Azid', 'conducteur')),
 QUALITES_LIST(QUALITES('securite', 2,5), QUALITES('Carbone', 2,5)) );
--- Q2/requete.sql
+++ Q2/requete.sql
@@ -3,3 +3,15 @@
 SELECT numVol, fonctionE, COUNT(*) as nb_personnes
 FROM VOL, TABLE(Equipage) e
 GROUP BY numVol, fonctionE;
+
+
+-- L’impact d’un indice de qualit e est donn ́e par le produit de sa valeur et du poids
+-- que lui est attribué. Pour chaque vol, indiquer l’impact de chaque indice de qualité
+SELECT numVol, nomQualite, indiceQualite * nb_votes AS impact
+FROM VOL, TABLE(IndicesQualite) iq
+ORDER BY numVol;
+
+-- Pour chaque indice de qualite, calculer son impact moyen
+SELECT nomQualite, AVG(indiceQualite * nb_votes) AS impact_moyen
+FROM VOL, TABLE(IndicesQualite) iq
+GROUP BY nomQualite;
