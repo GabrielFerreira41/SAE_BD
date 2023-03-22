24f48803d1ac78439bb9f367953a51c498dbbc55
FARAULT Theo <o22100566@info21-23.iut45.univ-orleans.fr>
2023-03-22 11:22:54 +0100

    ajout requete 1 pour Q2

--- Q2/bd.sql
+++ Q2/bd.sql
@@ -34,4 +34,4 @@ CREATE TABLE VOL(
 nested table Equipage store as Equipage_nt;
 
 INSERT INTO VOL values('2556', TO_DATE('2023-07-10 23:30:00', 'YYYY-MM-DD HH24:MI:SS'),TO_DATE('2023-07-11 05:30:00', 'YYYY-MM-DD HH24:MI:SS'), 'Charles de Gaulle','Antonio Carlos Jobim',
-    equipageTabT(EQUIPAGE('Jean', 'conducteur'), EQUIPAGE('George', 'aspirateurman')),QUALITES_LIST(QUALITES('securite', 2,5), QUALITES('Carbone', 2,5)) );
+    equipageTabT(EQUIPAGE('Jean', 'conducteur'), EQUIPAGE('George', 'femme de ménage')),QUALITES_LIST(QUALITES('securite', 2,5), QUALITES('Carbone', 2,5)) );
