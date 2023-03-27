4a3a73650057a05dc1d74c644fd66e6d01219bff
Theo <farault.theo@gmail.com>
2023-03-27 22:34:28 +0200

    ajout question pour q4

--- Q4/requete.sql
+++ Q4/requete.sql
@@ -3,6 +3,10 @@ MATCH (a1:AEROPORT {villeA: 'Paris'})-[:DEPART]->(v1:VOL)-[:ARRIVE]->(a2:AEROPOR
 
 -- 1
 MATCH (a1:AEROPORT {villeA: 'Paris'})-[:ITINERAIRE*]->(v1:VOL)-[:ITINERAIRE*]->(a2:AEROPORT)return distinct a2
+MATCH (a1:AEROPORT {villeA: 'Paris'})-[:ITINERAIRE*]->(v1:VOL)-[:ITINERAIRE*]->(a2:AEROPORT) WITH v1 MATCH (adep:AEROPORT)-[:ITINERAIRE]->(v1)-[:ITINERAIRE]->(aarr:AEROPORT) return distinct adep, v1.numVol, aarr
 
 -- 2 
-MATCH (a1:AEROPORT {villeA: 'Paris'})-[:ITINERAIRE*]->(v1:VOL)-[:ITINERAIRE*]->(a2:AEROPORT) where v1.dateheureARR<v1.dateheureDEP return distinct a2
+MATCH (a1:AEROPORT {villeA: 'Paris'})-[:ITINERAIRE*]->(v1:VOL)-[:ITINERAIRE*]->(a2:AEROPORT) WITH v1 MATCH (adep:AEROPORT)-[:ITINERAIRE]->(v1)-[:ITINERAIRE]->(aarr:AEROPORT)-[:ITINERAIRE]->(v2:VOL) where v1.dateheureARR<v2.dateheureDEP return distinct adep, v1, aarr, v2
+
+-- 3
+MATCH (a1:AEROPORT {villeA: 'Paris'})-[:ITINERAIRE*]->(v1:VOL)-[:ITINERAIRE*]->(a2:AEROPORT) WITH v1 MATCH (adep:AEROPORT)-[:ITINERAIRE]->(v1)-[:ITINERAIRE]->(aarr:AEROPORT)-[:ITINERAIRE]->(v2:VOL) where v1.dateheureARR<v2.dateheureDEP with adep, v1, aarr, v2 MATCH (aaa:AEROPORT) where adep.villeA<>'Paris' and (aaa=adep or aaa=aarr) return distinct aaa.nomA as villeintermediaire
