a00760cc0a5f1962201ff7a571890fcc25294322
FARAULT Theo <o22100566@info27-21.iut45.univ-orleans.fr>
2023-04-05 12:28:46 +0200

    avancement q3

--- Q3/datalog.sql
+++ Q3/datalog.sql
@@ -46,10 +46,36 @@ terminal(3a, londres-heathrow).
 terminal(4a, londres-heathrow).
 terminal(5a, londres-heathrow).
 
+inf(0,1).
+inf(1,2).
+inf(2,3).
+inf(3,4).
+inf(4,5).
+inf(5,6).
+inf(6,7).
+inf(7,8).
+inf(8,9).
+inf(9,10).
+inf(10,11).
+inf(11,12).
+inf(12,13).
+inf(13,14).
+inf(14,15).
+inf(15,16).
+inf(16,17).
+inf(17,18).
+inf(18,19).
+inf(19,20).
+inf(20,21).
+inf(21,22).
+inf(22,23).
+inf(23,24).
 
-vol(0442, 23, 05, 1,charles-de-gaule,antonio-carlos-jobim,2c,1).
-vol(0443,10,01,1,antonio-carlos-jobim,francisci-sa-carneiro,1,1f).
-vol(0444,05,20,1,francisci-sa-carneiro,pierre-eliot-trudeau,1f,1m).
+inf(X,Y) :- inf(X,Z), inf(Z,Y).
+
+vol(0442,1, 5, 1,charles-de-gaule,antonio-carlos-jobim,2c,1).
+vol(0443,6,7,1,antonio-carlos-jobim,francisci-sa-carneiro,1,1f).
+vol(0444,5,10,1,francisci-sa-carneiro,pierre-eliot-trudeau,1f,1m).
 vol(0445,05,07,1,adolfo-Suárez-de-madrid-barajas,londres-heathrow,1eS,1a).
 vol(0446,08,10,1,londres-heathrow,adolfo-Suárez-de-madrid-barajas,1a,1eS).
 
@@ -57,3 +83,18 @@ vol_possible(X, Y,A) :- vol(_, _, _, A, X, Y, _, _).
 vol_possible(X, Y,A) :- vol(_, _,D, _, X, Z, _, _), vol_possible(Z, Y,A), Y != X.
 
 ville_possible(D,A) :- vol_possible(X,Y,_),aeroport(X,_,D),aeroport(Y,_,A).
+
+
+connected(X, Y, DepartureTime, ArrivalTime) :-
+    vol(_, DepartureTime,ArrivalTime , _, X, Y, _, _),
+    aeroport(X,_,D),
+    aeroport(Y,_,A),
+    X!=Y.
+    
+connected(X, Y, DepartureTime1, ArrivalTime2) :-
+    connected(X, Z, DepartureTime1, ArrivalTime1),
+    connected(Z, Y, DepartureTime2, ArrivalTime2),
+    inf(ArrivalTime1, DepartureTime2),
+    X!=Y.
+
+    
